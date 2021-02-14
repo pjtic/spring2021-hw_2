3e9430b48f555f6a93ce3b064078d3e170ea1ca5
root <root@DESKTOP-927BVDH.localdomain>
2021-02-14 14:37:23 -0700

    updated main.c files for static matrix

---
+++ STATIC/main.c
@@ -0,0 +1,35 @@
+
+nclude "matrix.h"
+
+int main() 
+{
+	  static T data[] = {1,2,3,4};
+	    matrix  a,b,custom_Matrix;
+	    a = create_intvals(2,3,data);
+	    b = create_intvals(2,3);
+	    equate(&a,&b);
+	    printf("\n Matrix a:");
+	    matrix_print(a);
+	    printf("\n Matrix b:");
+	    matrix_print(b);
+	    printf("\n a+b");
+	    matrix_print(add(a,b));
+
+	    printf("\n Transposed Matrix a");
+	    matrix_print_integer(transpose(a));
+
+            printf("\n Transposed Matrix b");
+	    matrix_print_integer(transpose(b));
+	    
+	    printf("Please enter desired number of rows");
+	    scanf("%d", &custom_Matrix.row_dim);
+
+	    printf("Please enter desired number of colums");
+	    scanf("%d", &custom_Matrix.col_dim);
+	    
+	   custom_Matrix = create_custom_Matrix(custom_Matrix.row_dim, custom_Matrix.col_dim);
+	   
+	   printf(" \n Transposed Custom Matrix");
+	   matrix_print_integer(transpose(custom_Matrix));
+
+}
